393327ee66683b2d061990e95943d0d06507454d
aluengo <alberto@attomic.es>
2023-02-21 17:41:44 +0100

    added firebase and storage lib

--- app/src/main/java/com/dralsoft/inventory/ui/theme/Color.kt
+++ app/src/main/java/com/dralsoft/inventory/ui/theme/Color.kt
@@ -5,6 +5,6 @@ import androidx.compose.ui.graphics.Color
 val Green = Color(0xFF59981A)
 
 val MidnightBlue = Color(0xFF145DA0)
-val DarkBlue = Color(0xFF0C2D48)
+val DarkBlue = Color(0xFF114269)
 val Blue = Color(0xFF189AB4)
 val BabyBlue = Color(0xFFB1D4E0)
